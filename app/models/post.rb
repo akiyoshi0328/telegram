4113b8eb7accadabf477c1fcc846699324418cc3
akiyoshi0328 <akiyoshi.takahashi.50@gmail.com>
2019-12-03 15:23:30 +0900

    画像登録完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   has_one_attached :image
   attribute :new_image
 
-  before_save do
-    self.image = new_image = new_image if new_image
+    before_save do
+      self.image = new_image if new_image
     end
   end
+
